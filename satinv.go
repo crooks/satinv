cfa4af59c2533946bb74b8e57306366e82733561
Steve Crook <steve@mixmin.net>
2022-11-20 11:48:45 +0000

    Don't defer close the logfile in a function

--- satinv.go
+++ satinv.go
@@ -354,11 +354,19 @@ func timeTrack(start time.Time, name string) {
 	log.Infof("%s took %s", name, elapsed)
 }
 
-// initLogging does all the stuff required to configure logging.  As log is a global variable, the function returns nothing.
-func initLogging() {
+func main() {
+	var err error
+	flags = config.ParseFlags()
+	cfg, err = config.ParseConfig(flags.Config)
+	if err != nil {
+		log.Fatalf("Cannot parse config: %v", err)
+	}
 	loglev, err := loglevel.ParseLevel(cfg.Logging.LevelStr)
 	if err != nil {
 		log.Fatalf("Unable to set log level: %v", err)
+	}
+	if cfg.Logging.Journal && !jlog.Enabled() {
+		log.Warn("Cannot log to systemd journal")
 	}
 	if cfg.Logging.Journal && jlog.Enabled() {
 		log.Current = jlog.NewJournal(loglev)
@@ -379,16 +387,6 @@ func initLogging() {
 		log.Current = lgr
 		log.Debugf("Logging to file %s has been initialised at level: %s", cfg.Logging.Filename, cfg.Logging.LevelStr)
 	}
-}
-
-func main() {
-	var err error
-	flags = config.ParseFlags()
-	cfg, err = config.ParseConfig(flags.Config)
-	if err != nil {
-		log.Fatalf("Cannot parse config: %v", err)
-	}
-	initLogging()
 	// Time to do some real work
 	mkInventory()
 }
